48f6f0176bfa0bcbad74bdc8f23033de1c9d3312
Isham Mohamed <isham4you@gmail.com>
2017-08-11 15:05:24 +0530

    Intial commit

---
+++ RTutorials/script.R
@@ -0,0 +1,4 @@
+student_name <- c("mike", "jane", "robert") #vector of the students
+student_age <- c(23, 25, 28)
+student_level <- factor(c("MS", "BS", "MS"), levels = c("MS", "BS", 'Diploma'))
+student_gender <- factor(c("male", "female", "male"))
