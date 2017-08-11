5eac01fb018a6ffae961de519690c79519fca80e
Isham Mohamed <isham4you@gmail.com>
2017-08-11 15:07:33 +0530

    Lists

--- RTutorials/script.R
+++ RTutorials/script.R
@@ -2,3 +2,6 @@ student_name <- c("mike", "jane", "robert") #vector of the students
 student_age <- c(23, 25, 28)
 student_level <- factor(c("MS", "BS", "MS"), levels = c("MS", "BS", 'Diploma'))
 student_gender <- factor(c("male", "female", "male"))
+#************** List Practice
+student_Jane <- list(student_name[2], student_age[2], student_level[2], student_gender[2])
+students <- list(student_name, student_age, student_level, student_gender)
